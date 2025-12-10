d5798bb713a18edfba508e905ca83171fe850373
Namig <namig.pashayev1@gmail.com>
2025-12-10 11:04:46 +0400

    Update home page with hero and links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+import logo from '@/assets/logo.webp';
+import popcorn from '@/assets/popcorn.webp';
+import Image from 'next/image';
+import Link from 'next/link';
+import { MdOutlineLiveTv } from "react-icons/md";
+
 export default function Home() {
   return (
-    <div>
-
-    </div>
+    <main>
+      <header className='relative bg-[linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)),url("/img/header-bg.webp")] bg-center bg-cover py-92 h-screen flex flex-col gap-5 items-center justify-center'>
+        <Image src={logo} height={320} width={320} alt='logo' />
+        <h1 className='text-white text-5xl font-bold max-w-1/2 text-center'>All the movies you love. None of the complexity.</h1>
+        <Link href='/popular-movies' className='mt-10 flex items-center gap-2 text-white px-5 py-3 rounded-xl bg-red-600 text-xl hover:bg-red-700 transition-all duration-100'>
+          <span className=''>Explore popular movies</span>
+          <Image src={popcorn} height={32} width={32} alt='icon' className='rotate-15' />
+        </Link>
+        <Link href='/live-trending' className='flex items-center gap-2 text-white px-5 py-3 rounded-xl bg-green-600 text-xl hover:bg-green-700 transition-all duration-100'>
+          <span className=''>Live top 10 trending</span>
+          <MdOutlineLiveTv className='text-3xl' />
+        </Link>
+      </header >
+    </main>
   );
 }
