7fb80c3286c56f3e4fb06aa774f625cafccd7763
Namig <namig.pashayev1@gmail.com>
2025-12-10 22:09:59 +0400

    Styling updates for MovieCard

--- src/lib/components/shared/MovieCard.tsx
+++ src/lib/components/shared/MovieCard.tsx
@@ -12,14 +12,14 @@ export default function MovieCard({ movie, index }: Props) {
     return (
         <div className='relative shrink-0 hover:scale-102 transition-transform duration-200 hover:cursor-pointer w-max' key={movie.id}>
             <Image
-                className='rounded-2xl'
+                className='rounded-2xl max-lg:h-[280px] w-auto max-sm:h-40'
                 src={posterUrl}
                 width={220}
                 height={330}
                 alt="Movie Poster"
             />
             <div
-                className='text-stroke absolute text-[160px] -left-10 font-bold text-black bottom-8 leading-none'>
+                className='text-stroke absolute text-[160px] -left-10 font-bold text-black bottom-8 leading-none max-lg:text-[120px] max-lg:-left-7 max-sm:text-[80px] max-sm:-left-6 max-sm:bottom-4'>
                 {index + 1}
             </div>
         </div>
